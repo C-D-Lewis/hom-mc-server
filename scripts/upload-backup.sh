a21ec2e317d0f2709127427d574f5ea62f2539ef
C-D-Lewis <Bonsitm@gmail.com>
2023-12-08 12:37:15 +0000

    update

--- scripts/upload-backup.sh
+++ scripts/upload-backup.sh
@@ -9,4 +9,4 @@ BUCKET_DIR="s3://public-files.chrislewis.me.uk/chunky-fargate/worlds"
 rm -rf ./*.zip
 zip -r $FILE .
 /usr/local/bin/aws s3 cp $FILE "$BUCKET_DIR/"
-mv $FILE last.zip
+mv $FILE /home/pi/last.zip
