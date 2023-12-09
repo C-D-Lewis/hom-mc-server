1f89764148b736eac356529c691b1be41c9d46aa
C-D-Lewis <Bonsitm@gmail.com>
2023-12-09 18:07:03 +0000

    update

--- scripts/local-backup.sh
+++ scripts/local-backup.sh
@@ -5,6 +5,14 @@ set -eu
 DIR="/mnt/ssd/"
 FILE="hom-mc-server.tar"
 
+if pgrep -x java >/dev/null
+then
+    sleep 1
+else
+    echo ">>> java is not running, world might not be launchable"
+    exit 1
+fi
+
 echo ">>> Updating ownership"
 chown -R pi $DIR
 
@@ -12,6 +20,9 @@ cd $DIR
 
 echo ">>> Creating archive"
 tar cf $FILE "$DIR/hom-mc-server"
+
+SIZE=$(stat -c '%s' $FILE | numfmt --to=si --suffix=B)
+echo ">>> Size: $SIZE"
 
 echo ">>> Moving"
 mv $FILE /mnt/usb/backup/
--- scripts/upload-backup.sh
+++ scripts/upload-backup.sh
@@ -6,11 +6,22 @@ DATE=$(TZ=GMT date +"%Y%m%d")
 FILE="hom-mc-server-$DATE.zip"
 BUCKET_DIR="s3://public-files.chrislewis.me.uk/chunky-fargate/worlds"
 
+if pgrep -x java >/dev/null
+then
+    sleep 1
+else
+    echo ">>> java is not running, world might not be launchable"
+    exit 1
+fi
+
 echo ">>> Removing zips"
 rm -rf ./*.zip
 
 echo ">>> Creating zip"
 zip -r $FILE . || true # Some files could not be read
+
+SIZE=$(stat -c '%s' $FILE | numfmt --to=si --suffix=B)
+echo ">>> Size: $SIZE"
 
 echo ">>> Uploading"
 /usr/local/bin/aws s3 cp $FILE "$BUCKET_DIR/"
